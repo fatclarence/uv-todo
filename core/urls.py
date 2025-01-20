4e19e7f3dc5557f41534afccd0f5e19718fe0e1c
Clarence K <ckoh.clarence.96@gmail.com>
2025-01-20 17:45:10 +0800

    add ability to  add new todos

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("tasks/", views.tasks, name="tasks"),
+    path("tasks/<int:task_id>/", views.toggle_todo, name="toggle_todo"), # <-- NEW
 ]
--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import redirect, render
 from .models import UserProfile, Todo
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 
 def index(request):
     return redirect("tasks/")
@@ -11,10 +12,33 @@ def get_user_todos(user: UserProfile) -> list[Todo]:
     return user.todos.all().order_by("created_at")
 
 @login_required
+@require_http_methods(["GET", "POST"])
 def tasks(request):
+    if request.method == "POST":
+        return _create_todo(request)
+    
     context = {
         "todos": get_user_todos(request.user),
         "fullname": request.user.get_full_name() or request.user.username,
     }
 
     return render(request, "tasks.html", context)
+
+@login_required
+@require_http_methods(["PUT"])
+def toggle_todo(request, task_id):
+    todo = request.user.todos.get(id=task_id)
+    todo.is_completed = not todo.is_completed
+    todo.save()
+
+    return render(request, "tasks.html#todo-item-partial", {"todo": todo})
+
+
+def _create_todo(request):
+    title = request.POST.get("title")
+    if not title:
+        raise ValueError("Title is required")
+
+    todo = Todo.objects.create(title=title, user=request.user)
+    return render(request, "tasks.html#todo-item-partial", {"todo": todo})
+
